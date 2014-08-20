605cfb442226a05a27119e8c3147363a7cf9ddf4
definetux <goncharuk.m.n@gmail.com>
2014-08-20 19:15:27 +0300

    Add some changes

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ $(document).ready(function() {
 		var $rootElement = this;
 		var $currentItem = null;
 		var $draggableStub = $('<li>').toggleClass('draggable-item', true).toggleClass('draggable-stub', true);
-
+		var $lastElement = $('<li>').toggleClass('draggable-item-shaper', true);
+		var timerId = null;
+		
 		this.find('.draggable-list > li').each(function (index, item) {
 			var $item = $(item);
 			$item.toggleClass('draggable-item', true);
 			$item.mousedown(function (event) {
 				$currentItem = $(this);
+				if($currentItem.parent().children().length == 1) {
+					$lastElement.insertAfter($currentItem);
+				}
 				$draggableStub.insertAfter($currentItem);
 				$currentItem.toggleClass('dragging', true);
 				_setRelativePosition(event);
@@ -23,7 +28,18 @@ $(document).ready(function() {
 				if($elem) {
 					var childPos = $currentItem.offset();
 					var parentPos = $draggableStub.parent().offset();
+					clearTimeout(timerId);
+					if(!$elem.hasClass('draggable-stub')) {
+						timerId = setTimeout(function() {
+							if($currentItem && $elem) {
+								$elem.text($elem.text() + ' ' + $currentItem.text());
+								$currentItem.detach();
+								$currentItem = null;
+							}
+						}, 2000);
+					}
 					if(childPos && parentPos && childPos.top - parentPos.top < $currentItem.outerHeight() / 2) {
+						
 						$draggableStub.insertBefore($elem);
 					} else {
 						$draggableStub.insertAfter($elem);
@@ -34,11 +50,17 @@ $(document).ready(function() {
 
 		$(document).mouseup(function() {
 			if($currentItem) {
+				if($draggableStub.parent().children().length == 2) {
+					$lastElement.detach();
+				}
+				$currentItem.trigger('OnDropped', [$currentItem.parent().attr('name'), $draggableStub.parent().attr('name'), $currentItem.text()]);
 				$currentItem.removeAttr('style');
 				$currentItem.detach();
 				$currentItem.insertAfter($draggableStub);
 				$currentItem.toggleClass('dragging', false);
 				$currentItem = null;
+				
+				
 			}
 			$draggableStub.detach();
 		});
@@ -62,9 +84,12 @@ $(document).ready(function() {
 			$currentItem.hide();
 			var $elem = $(document.elementFromPoint(x,y));
 			$currentItem.show();
-			return $elem.closest('.draggable-item:not(.dragging.draggable-stub)');
+			return $elem.closest('.draggable-item-shaper,.draggable-item:not(.dragging.draggable-stub)');
 		}
 	};
 
 	$('.draggable').makeDraggable();
+	$('.draggable').on('OnDropped', function(e, from, to, name) {
+		console.log('Element ' + name + ' has been dragged from ' + from + ' to ' + to);
+	});
 });
