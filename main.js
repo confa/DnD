9a89ea648fda3cd8cb9a1a511ddf28130752cb2c
definetux <goncharuk.m.n@gmail.com>
2014-08-22 11:03:04 +0300

    Refactor and handle event

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
                         for (i = 0; i < $currentItem.children().length; i++) {
                             $items += $($currentItem.children()[i]).html() + " ";
                         }
-                        $currentItem.trigger('MoveItem', [$movingInfo, $draggableStub.parent().attr("name")]);
+                        $currentItem.trigger('OnDropped', [$movingInfo, $draggableStub.parent().attr("name")]);
                         $movingInfo.data.length = 0;
 
                         $currentItem.removeAttr('style');
@@ -147,7 +147,7 @@ $(document).ready(function () {
                         $('.draggable-item-selected').toggleClass('draggable-item-selected', false)
                         $currentItem = null;
                     } else if (!$currentItem.hasClass('draggable-item-selected') && $currentItem.hasClass('dragging')) {
-                        $currentItem.trigger('MoveItem', [$movingInfo, $draggableStub.parent().attr("name")]);
+                        $currentItem.trigger('OnDropped', [$movingInfo, $draggableStub.parent().attr("name")]);
                         $movingInfo.data.length = 0;
                         $currentItem.insertAfter($draggableStub);
                         $currentItem.toggleClass('dragging', false);
@@ -194,6 +194,7 @@ $(document).ready(function () {
                             for (i = 0; i < $currentItem.children().length; i++)
                                 mergeTo.text(mergeTo.html() + ' ' + $($currentItem.children()[i]).html());
                             $currentItem.removeAttr('style');
+							mergeTo.trigger('OnCombined', [mergeTo.text()]);
                             $currentItem.children().detach();
                             $currentItem.toggleClass('dragging', false);
                             $('.draggable-item-selected').toggleClass('draggable-item-selected', false)
@@ -209,9 +210,12 @@ $(document).ready(function () {
     };
 
     $('.draggable').makeDraggable();
-    $('.draggable').on('MoveItem', function (e, info, to) {
+    $('.draggable').on('OnDropped', function (e, info, to) {
         for (i = 0; i < info.data.length; i++) {
             console.log("Track " + info.data[i].item + " was moved from playlist with name: " + info.data[i].name + ", to playlist with name: " + to);
         }
     });
+	$('.draggable').on('OnCombined', function(e, combinedText) {
+		console.log(combinedText);
+	});
 });
