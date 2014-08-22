7a968f7a6ac28bc2bcabd1b0b9ab5eef99929164
Beseda Dmitriy <BesedaDG@gmail.com>
2014-08-22 16:49:58 +0300

    Refactor

--- main.js
+++ main.js
@@ -1,63 +1,60 @@
 $(document).ready(function () {
 
     $.fn.makeDraggable = function(options) {
-        var $mousedown = false;
         var $rootElement = this;
         var $currentItem = null;
         var $draggableStub = $('<li>').toggleClass('draggable-item', true).toggleClass('draggable-stub', true);
         var $container = $('#container');
-        var $parentId = "";
-        var $movingInfo = { "data": [] };
-        var $timerId = null;
+        var parentId = "";
+        var movingInfo = { "data": [] };
+        var timerId = null;
         var lastEvent = null;
-
-        var $elementOffset = { x: 0, y: 0 };
+        var elementOffset = { x: 0, y: 0 };
+        var mousedown = false;
 
         this.find('.draggable-list > li').each(function(index, item) {
             var $item = $(item);
             $item.toggleClass('draggable-item', true);
             $item.mousedown(function(event) {
-                $mousedown = true;
+                mousedown = true;
                 $currentItem = $(this);
-
-                var $pos = $currentItem.offset();
-                $elementOffset.x = event.pageX - $pos.left;
-                $elementOffset.y = event.pageY - $pos.top;
-
+                var pos = $currentItem.offset();
+                elementOffset.x = event.pageX - pos.left;
+                elementOffset.y = event.pageY - pos.top;
 
                 if (!$currentItem.hasClass('draggable-item-selected') && !event.shiftKey) {
-                    $movingInfo.data.push({ "name": $currentItem.parent().attr('name'), "item": $currentItem.html() });
-                };
+                    movingInfo.data.push({ "name": $currentItem.parent().attr('name'), "item": $currentItem.html() });
+                }
 
                 if (event.ctrlKey) {
                     if ($currentItem.hasClass('draggable-item-selected')) {
                         $currentItem.toggleClass('draggable-item-selected', false);
-                        for (i = 0; i < $movingInfo.data.length; i++) {
-                            if ($movingInfo.data[i].name == $currentItem.parent().attr('name') && $movingInfo.data[i].item == $currentItem.html()) {
-                                $movingInfo.data.splice(i, 1);
+                        for (i = 0; i < movingInfo.data.length; i++) {
+                            if (movingInfo.data[i].name == $currentItem.parent().attr('name') && movingInfo.data[i].item == $currentItem.html()) {
+                                movingInfo.data.splice(i, 1);
                             }
                         }
                     } else {
                         $currentItem.toggleClass('draggable-item-selected', true);
                     }
                 } else if (event.shiftKey) {
-                    var $indexFirst = -1;
-                    var $indexLast = -1;
+                    var indexFirst = -1;
+                    var indexLast = -1;
                     if ($('.draggable-item-selected').index() < $currentItem.index()) {
-                        $indexFirst = $('.draggable-item-selected').index();
-                        $indexLast = $currentItem.index();
+                        indexFirst = $('.draggable-item-selected').index();
+                        indexLast = $currentItem.index();
                     } else {
-                        $indexFirst = $currentItem.index();
-                        $indexLast = $('.draggable-item-selected').index();
+                        indexFirst = $currentItem.index();
+                        indexLast = $('.draggable-item-selected').index();
                     }
-					
-					var currentChildren = $currentItem.parent().children();
-					
-                    for (i = $indexFirst; i <= $indexLast; i++) {
-						var $currentChild = $(currentChildren[i]);
+
+                    var currentChildren = $currentItem.parent().children();
+
+                    for (i = indexFirst; i <= indexLast; i++) {
+                        var $currentChild = $(currentChildren[i]);
                         if (!$currentChild.hasClass('draggable-item-selected')) {
                             $currentChild.toggleClass('draggable-item-selected', true);
-                            $movingInfo.data.push({ "name": $currentChild.parent().attr('name'), "item": $currentChild.html() });
+                            movingInfo.data.push({ "name": $currentChild.parent().attr('name'), "item": $currentChild.html() });
                         }
                     }
                 } else {
@@ -72,73 +69,66 @@ $(document).ready(function () {
                     }
                     $currentItem.toggleClass('draggable-item-selected', true);
                 }
-                $parentId = $currentItem.parent().attr('id');
+                parentId = $currentItem.parent().attr('id');
             });
         });
 
         this.mousemove(function(event) {
-        	lastEvent = event;
-            if ($currentItem && $mousedown) {
+            lastEvent = event;
+            if ($currentItem && mousedown) {
                 var listCount = $currentItem.parent().children().length
 
                 if ($currentItem.hasClass('draggable-item-selected')) {
                     $currentItem = $container;
 
                     $currentItem.append($('.draggable-item-selected'));
-                };
+                }
                 $currentItem.toggleClass('dragging', true);
 
-                if (!$currentItem.hasClass('container')) { // for not creating $draggableStub in container
+                if (!$currentItem.hasClass('container')) {
                     $draggableStub.insertAfter($currentItem.children().last());
-                };
+                }
                 $currentItem.toggleClass('dragging', true);
-
                 _setRelativePosition(event);
                 var $elem = _getCurrentTarget(event);
                 if ($elem) {
-					if ($elem.hasClass('draggable-list'))
-					{
-						$elem.append($draggableStub);
-					}
-					else 
-					{
-						var childPos = $currentItem.offset();
-						var parentPos = $draggableStub.parent().offset();
+                    if ($elem.hasClass('draggable-list')) {
+                        $elem.append($draggableStub);
+                    } else {
+                        var childPos = $currentItem.offset();
+                        var parentPos = $draggableStub.parent().offset();
 
-						if ($elem.length > 0 && $mousedown) {
-							clearTimeout($timerId);
-							var $savedItem = $currentItem;
-							$timerId = setTimeout(function() {
-								var $hoverElement = _getCurrentTarget(lastEvent);
-								if($mousedown && $elem.is($hoverElement)) {
-									$elem.trigger('MergeItems', [$elem, $savedItem, event.pageY]);
-								}
-							}, 2000);
-							if (!$elem.hasClass('draggable-stub-empty')) {
-								if (childPos && parentPos && childPos.top - parentPos.top < $($currentItem.children[0]).outerHeight() / 2) {
-									$draggableStub.insertBefore($elem);
-								} else {
-									$draggableStub.insertAfter($elem);
-								}
-							}
-						}
-					}
-                };
+                        if ($elem.length > 0 && mousedown) {
+                            clearTimeout(timerId);
+                            var $savedItem = $currentItem;
+                            timerId = setTimeout(function() {
+                                var $hoverElement = _getCurrentTarget(lastEvent);
+                                if (mousedown && $elem.is($hoverElement)) {
+                                    $elem.trigger('MergeItems', [$elem, $savedItem, event.pageY]);
+                                }
+                            }, 2000);
+                            if (!$elem.hasClass('draggable-stub-empty')) {
+                                if (childPos && parentPos && childPos.top - parentPos.top < $($currentItem.children[0]).outerHeight() / 2) {
+                                    $draggableStub.insertBefore($elem);
+                                } else {
+                                    $draggableStub.insertAfter($elem);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         });
 
         $(document).mouseup(function() {
-            $mousedown = false;
-            clearTimeout($timerId);
+            mousedown = false;
+            clearTimeout(timerId);
             if ($currentItem) {
-
                 var $element = _getCurrentTarget(event);
-
                 if ($element.length == 0) {
                     if ($currentItem.hasClass('container')) {
-                        $('#' + $parentId).append($currentItem.children())
+                        $('#' + parentId).append($currentItem.children())
                         $currentItem = null;
-
                     } else {
                         $currentItem.removeAttr('style');
                         $currentItem.toggleClass('dragging', false);
@@ -147,41 +137,36 @@ $(document).ready(function () {
 
                     if ($element.hasClass('draggable-stub-empty')) {
                         $element.replaceWith($draggableStub);
-                    };
+                    }
 
                     if ($currentItem.children().length > 0) {
                         var $items = "Items "
                         for (i = 0; i < $currentItem.children().length; i++) {
                             $items += $($currentItem.children()[i]).html() + " ";
                         }
-                        $currentItem.trigger('OnDropped', [$movingInfo, $draggableStub.parent().attr("name")]);
-                        $movingInfo.data.length = 0;
-
+                        $currentItem.trigger('OnDropped', [movingInfo, $draggableStub.parent().attr("name")]);
+                        movingInfo.data.length = 0;
                         $currentItem.removeAttr('style');
-
                         $currentItem.children().insertAfter($draggableStub);
                         $currentItem.toggleClass('dragging', false);
                         $('.draggable-item-selected').toggleClass('draggable-item-selected', false)
                         $currentItem = null;
                     } else if (!$currentItem.hasClass('draggable-item-selected') && $currentItem.hasClass('dragging')) {
-                        $currentItem.trigger('OnDropped', [$movingInfo, $draggableStub.parent().attr("name")]);
-                        $movingInfo.data.length = 0;
+                        $currentItem.trigger('OnDropped', [movingInfo, $draggableStub.parent().attr("name")]);
+                        movingInfo.data.length = 0;
                         $currentItem.insertAfter($draggableStub);
                         $currentItem.toggleClass('dragging', false);
                         $currentItem = null;
                     }
-                };
-            };
-            // if ($('#' + $parentId).children().length < 1) {
-                // $('#' + $parentId).append($draggableStubEmpty);
-            // };
+                }
+            }
             $draggableStub.detach();
         });
 
         function _setRelativePosition(event) {
-        	var $parentOffet = $rootElement.offset();
-            var relX = event.pageX - $elementOffset.x - $parentOffet.left;
-            var relY = event.pageY - $elementOffset.y - $parentOffet.top
+            var parentOffet = $rootElement.offset();
+            var relX = event.pageX - elementOffset.x - parentOffet.left;
+            var relY = event.pageY - elementOffset.y - parentOffet.top
             $currentItem.css({
                 'top': relY + 'px',
                 'left': relX + 'px'
@@ -200,22 +185,21 @@ $(document).ready(function () {
             if ($elem.hasClass('draggable-stub-empty')) {
                 return $elem;
             }
-			if ($elem.closest('.draggable-list').find('li').length === 0)
-			{
-				return $elem.closest('.draggable-list');
-			}
+            if ($elem.closest('.draggable-list').find('li').length === 0) {
+                return $elem.closest('.draggable-list');
+            }
             return $elem.closest('.draggable-item:not(.dragging.draggable-stub)');
         }
 
         $('.draggable').on('MergeItems', function(e, mergeTo, mergeElem, pageY) {
-			mergeToPos = mergeTo.offset();
-            if ((!mergeTo.hasClass('draggable-stub')) && (mergeToPos.top + 0.15*$('.draggable-item').height() < pageY) && (mergeToPos.top + 0.85*$('.draggable-item').height() > pageY)) {
+            mergeToPos = mergeTo.offset();
+            if ((!mergeTo.hasClass('draggable-stub')) && (mergeToPos.top + 0.15 * $('.draggable-item').height() < pageY) && (mergeToPos.top + 0.85 * $('.draggable-item').height() > pageY)) {
                 if (mergeElem && mergeTo) {
                     if ($currentItem.hasClass('container') && $('.draggable-item-selected').length > 0) {
                         for (i = 0; i < $currentItem.children().length; i++)
                             mergeTo.text(mergeTo.html() + ' ' + $($currentItem.children()[i]).html());
                         $currentItem.removeAttr('style');
-						mergeTo.trigger('OnCombined', [mergeTo.text()]);
+                        mergeTo.trigger('OnCombined', [mergeTo.text()]);
                         $currentItem.children().detach();
                         $currentItem.toggleClass('dragging', false);
                         $('.draggable-item-selected').toggleClass('draggable-item-selected', false)
@@ -235,7 +219,7 @@ $(document).ready(function () {
             console.log("Track " + info.data[i].item + " was moved from playlist with name: " + info.data[i].name + ", to playlist with name: " + to);
         }
     });
-	$('.draggable').on('OnCombined', function(e, combinedText) {
-		console.log(combinedText);
-	});
+    $('.draggable').on('OnCombined', function (e, combinedText) {
+        console.log(combinedText);
+    });
 });
